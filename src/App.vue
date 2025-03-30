f3101a5caab3a8c8ceae5c1aa34db009c6c99bbf
BigCookie233 <86756026+BigCookie233@users.noreply.github.com>
2025-03-30 21:52:54 +0800

    feat: 实现主要框架

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="mdc-card__title">大标题</div>
+  <div class="mdc-card__subtitle"><p>小标题</p></div>
+</template>
+
+<style scoped lang="scss">
+</style>
